8eb78d6671b887c0fbd4c5d20f41a9843cb369f6
Moon <Vaccaria1573@users.noreply.github.com>
2018-10-24 16:06:29 +0800

    xx

--- galaxz2/js/funds.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("img.lazy").lazyload({
-        placeholder : "./images/crowdfund/funds_begin.jpg",
-        effect : "fadeIn",
-        threshold : 200,
-        container : $(".funds-container"),
-    });
-});
--- galaxz2/js/shop.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("img.lazy").lazyload({
-        placeholder : "http://html5resource-10019461.file.myqcloud.com/galaxz/images/crowdfund/shop_begin.jpg",
-        effect : "fadeIn",
-        threshold : 200,
-        container : $(".shop-container"),
-    });
-});
